d3c83325fdc868f6b63de48cf3f8d9bb8ab6b69f
Emma Ahrens <emmakatherina@icloud.com>
2018-06-16 18:38:19 +0200

    SimpleDistanceGraph
    Create class SimpleDistanceGraph, make reading from files possible
    and add simple tests.

--- Aufgabe5/distance_graph.cpp
+++ Aufgabe5/distance_graph.cpp
@@ -3,15 +3,29 @@
 #include "distance_graph.h"
 #include <fstream>
 
+using std::cout;
+using std::endl;
+
 int main () {
     std::cout << "Hello World!" << std::endl;
     
+    // Tests for class CoordinateGraph
     std::ifstream in ("daten/Graph1.dat");
-    CoordinateGraph graph;
-    double a;
-    in >> graph;
-    graph.addEdge(2,3,0.7);
-    graph.print();
+    CoordinateGraph graph1;
+    in >> graph1;
+    in.close();
+
+    std::cout << "Estimated cost 1-2: " << graph1.estimatedCost(1,2) << std::endl;
+    std::cout << "Real cost 1-2: " << graph1.cost(1,2) << endl;
+    
+    //Tests for class 
+    in.open("daten/Graph3.dat");
+    SimpleDistanceGraph graph2 (1500);
+    in >> graph2;
+    in.close();
+
+    std::cout << "Estimated cost 2-3: " << graph2.estimatedCost(2,3) << endl;
+    std::cout << "Real cost 2-3: " << graph2.cost(2,3) << endl;
 
     return 0;
 }
--- Aufgabe5/distance_graph.h
+++ Aufgabe5/distance_graph.h
@@ -52,6 +52,7 @@ class DistanceGraph
 };
 //-------------------------------------------------------------------------------------------------
 // Klasse zur Implementierung von Graphen, für deren Knoten die Position durch Koordinaten gegeben ist
+// Benutze diese Klasse für die Graphen 1 und 2 (Bei Graph 2 müsste man sich das nochmal überlegen! TODO)
 //-------------------------------------------------------------------------------------------------
 
 class CoordinateGraph : public DistanceGraph {
@@ -91,11 +92,10 @@ class CoordinateGraph : public DistanceGraph {
         eastWest = new double [num_verts];
     };
 
-    void addEdge(VertexT from, VertexT to, double cost) {
-        EdgeT edge;
+    void addEdge(VertexT from, VertexT to, CostT cost) {
+        LocalEdgeT edge;
         edge.first = to;
         edge.second = cost;
-        std::cout << edge.second << "X";
         vertices[from].push_back(edge);
     }
 
@@ -120,11 +120,16 @@ class CoordinateGraph : public DistanceGraph {
 
     void print() {
         std::cout << "Printing Graph:\n";
+        std::cout << "Edges:\n";
         for (std::size_t i = 0; i < vertexCount; i++) {
             std::cout << i;
             for (std::size_t j = 0; j < vertices[i].size(); j++) {
                 std::cout << "\t" << vertices[i][j].second << " " << vertices[i][j].first << std::endl;
             }
+        }
+        std::cout << "Coordinates:\n";
+        for (std::size_t i = 0; i < vertexCount; i++) {
+            std::cout << i << " " << northSouth[i] << " " << eastWest[i] << std::endl;
         }
     };
 };
@@ -132,7 +137,6 @@ class CoordinateGraph : public DistanceGraph {
 std::istream& operator>> (std::istream& in, CoordinateGraph& graph) {
     int num_verts, num_edges;
     in >> num_verts >> num_edges;
-    std::cout << num_verts << num_edges;
     graph.resize(num_verts);
 
     for (int i = 0; i < num_edges; i++) {
@@ -140,7 +144,6 @@ std::istream& operator>> (std::istream& in, CoordinateGraph& graph) {
         double cost;
 
         in >> from >> to >> cost;
-        std::cout << from << to << cost << std::endl;
         graph.addEdge(from, to, cost);
 
     }
@@ -155,7 +158,113 @@ std::istream& operator>> (std::istream& in, CoordinateGraph& graph) {
 }
 
 //-------------------------------------------------------------------------------------------------
-// Klasse zur Implementierung von Graphen, für deren Knoten die Position durch Koordinaten gegeben ist
+// Klasse zur Implementierung von Graphen, deren Knotenabstände in Kilometer oder Minuten gegeben ist.
+// Benutze diese Klasse für Graphen 3 und 4.
 //-------------------------------------------------------------------------------------------------
+
+class SimpleDistanceGraph : public DistanceGraph {
+  private:
+    std::vector<NeighborT> vertices;
+    CostT edge_max;
+
+    //typedef std::size_t                 VertexT;
+    //typedef std::pair<VertexT,VertexT>  EdgeT;
+    //typedef double                      CostT;
+    //typedef std::pair<VertexT,CostT>  LocalEdgeT;
+    //typedef std::vector<LocalEdgeT>   NeighborT;
+  public:
+    SimpleDistanceGraph(double edge_max, int num_verts = 0) : DistanceGraph(num_verts) {//Copied
+        (*this).edge_max = edge_max;
+        NeighborT dummy;
+        vertices.resize(num_verts, dummy);
+    }
+
+    //Destruktor notwenig? TODO
+
+    void resize(int num_verts) {//Copied
+        vertexCount = num_verts;
+        NeighborT dummy;
+        vertices.resize(num_verts, dummy);
+    };
+
+    void addEdge(VertexT from, VertexT to, CostT cost) {//Copied
+        LocalEdgeT edge;
+        edge.first = to;
+        edge.second = cost;
+        vertices[from].push_back(edge);
+    }
+
+    const NeighborT& getNeighbors(VertexT v) const override {
+        if (vertices.size() > v) {
+            return vertices[v];
+        } else {
+            std::cerr << "Fehler in SimpleDistanceGraph::getNeighbors(VertexT): Ungültiger Index!\n";
+            exit(0);
+        }
+    };
+    
+    CostT estimatedCost( VertexT from, VertexT to) const override {
+        CostT c = cost(from, to);
+
+        if (c == 100000) {   //Change to infty TODO
+            return edge_max;
+        } else {
+            return c;
+        }
+    };
+
+    CostT cost( VertexT from, VertexT to) const override {
+        NeighborT neighbors = vertices[from];
+        for (auto edge : neighbors) {
+            if (edge.first == to) {
+                return edge.second;
+            }
+        }
+        return 100000.; //Change to infty TODO
+    };
+
+    void print() {
+        std::cout << "Printing Graph:\n";
+        std::cout << "Edges:\n";
+        for (std::size_t i = 0; i < vertexCount; i++) {
+            std::cout << i;
+            for (std::size_t j = 0; j < vertices[i].size(); j++) {
+                std::cout << "\t" << vertices[i][j].second << " " << vertices[i][j].first << std::endl;
+            }
+        }
+    };
+};
+
+std::istream& operator>> (std::istream& in, SimpleDistanceGraph& graph) {
+    int num_verts, num_edges;
+    in >> num_verts >> num_edges;
+    graph.resize(num_verts);
+
+    for (int i = 0; i < num_edges; i++) {
+        size_t from, to;
+        double cost;
+
+        in >> from >> to >> cost;
+        graph.addEdge(from, to, cost);
+
+    }
+
+    return in;
+}
+
+//-------------------------------------------------------------------------------------------------
+// Abstrakte Basisklasse zur Repraesentation eines Distanzgraphen
+//-------------------------------------------------------------------------------------------------
+
+/*class MazeGraph : public DistanceGraph {
+  public:
+    DistanceGraph(int num_verts = 0);
+
+    //Destruktor notwenig?
+
+    const NeighborT& getNeighbors( VertexT v) const override;
+    CostT estimatedCost( VertexT from, VertexT to) const override;
+    CostT cost( VertexT from, VertexT to) const override;
+};*/
 
 #endif

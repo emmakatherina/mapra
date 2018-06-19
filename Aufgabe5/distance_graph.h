03174168944148f2eca847aefb4120b1790b77ee
Emma Ahrens <emmakatherina@icloud.com>
2018-06-19 21:13:26 +0200

    Adapt current code to work nicelyt with unit.h

--- Aufgabe5/distance_graph.h
+++ Aufgabe5/distance_graph.h
@@ -3,53 +3,10 @@
 #ifndef _DISTANCE_GRAPH_
 #define _DISTANCE_GRAPH_
 
-#include <functional>
-#include <list>
-#include <utility>
-#include <vector>
+#include "unit.h"
 #include <iostream>
 #include <cmath>
 
-typedef std::size_t                 VertexT;
-typedef std::pair<VertexT,VertexT>  EdgeT;
-typedef double                      CostT;
-
-extern const double infty;
-extern const VertexT undefinedVertex;
-
-//-------------------------------------------------------------------------------------------------
-// Abstrakte Basisklasse zur Repraesentation eines Distanzgraphen
-//-------------------------------------------------------------------------------------------------
-
-class DistanceGraph
-{
-  public:
-    typedef std::pair<VertexT,CostT>  LocalEdgeT;
-    typedef std::vector<LocalEdgeT>   NeighborT;
-    
-  protected:
-    // Die Anzahl der Knoten im Graphen.
-    std::size_t vertexCount;
-    
-  public:
-    DistanceGraph( int num_verts= 0)
-      : vertexCount(num_verts) {}
-      
-    virtual ~DistanceGraph() {}
-
-    // Gibt die Anzahl der Knoten im Graphen zurueck.
-    std::size_t numVertices() const { return vertexCount; }
-
-    // Gibt die Nachbarknoten eines gegebenen Knotens v zurueck.
-    virtual const NeighborT& getNeighbors( VertexT v) const = 0;
-    
-    // Heuristik: Schaetzt die Kosten, um vom Knoten "from" zum Knoten "to" zu gelangen.
-    virtual CostT estimatedCost( VertexT from, VertexT to) const = 0;
-    
-    // Gibt die wahren Kosten c an, um vom Knoten "from" zum Knoten "to" zu gelangen.
-    // Existiert zwischen den beiden Knoten keine Kante, werden die Kosten mit infty bewertet.
-    virtual CostT cost( VertexT from, VertexT to) const = 0;
-};
 //-------------------------------------------------------------------------------------------------
 // Klasse zur Implementierung von Graphen, für deren Knoten die Position durch Koordinaten gegeben ist
 // Benutze diese Klasse für die Graphen 1 und 2 (Bei Graph 2 müsste man sich das nochmal überlegen! TODO)
@@ -115,7 +72,7 @@ class CoordinateGraph : public DistanceGraph {
                 return edge.second;
             }
         }
-        return 100000.; //Change to infty TODO
+        return infty;
     };
 
     void print() {
@@ -206,7 +163,7 @@ class SimpleDistanceGraph : public DistanceGraph {
     CostT estimatedCost( VertexT from, VertexT to) const override {
         CostT c = cost(from, to);
 
-        if (c == 100000) {   //Change to infty TODO
+        if (c == infty) {
             return edge_max;
         } else {
             return c;
@@ -220,7 +177,7 @@ class SimpleDistanceGraph : public DistanceGraph {
                 return edge.second;
             }
         }
-        return 100000.; //Change to infty TODO
+        return infty;
     };
 
     void print() {
@@ -335,7 +292,7 @@ class MazeGraph : public DistanceGraph {
     };
 
     CostT estimatedCost( VertexT from, VertexT to) const override {
-        if (cost(from, to) != 100000) { //TODO change to infty
+        if (cost(from, to) != infty) {
             return cost(from, to);
         } else {
             indexT fromI = vertexToIndex(from);
@@ -361,7 +318,7 @@ class MazeGraph : public DistanceGraph {
                 }
             }
         }
-        return 100000;
+        return infty;
     };
 };
 
